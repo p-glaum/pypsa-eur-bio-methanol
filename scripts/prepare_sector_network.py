2d1c135a8e7cb834b3a17e3b6e9a4ac1d55e5a3a
Philipp Glaum <p.glaum@tu-berlin.de>
2024-10-25 14:34:10 +0200

    update FT

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -3531,23 +3531,22 @@ def add_industry(n, costs):
                     lifetime=costs.at["decentral oil boiler", "lifetime"],
                 )
 
+    total_efficiency = 1 / costs.at["Fischer-Tropsch", "hydrogen-input"]
     n.madd(
         "Link",
         nodes + " Fischer-Tropsch",
         bus0=nodes + " H2",
         bus1=spatial.oil.nodes,
-        bus2=spatial.co2.nodes,
+        bus2=spatial.oil.kerosene,
+        bus3=spatial.co2.nodes,
         carrier="Fischer-Tropsch",
-        efficiency=1 / costs.at["Fischer-Tropsch", "hydrogen-input"],
+        efficiency=total_efficiency
+        * 0.6,  # fraction naptha and diesel because cracking of naptha is done separately
+        efficiency2=total_efficiency * 0.4,  # fraction kerosene
+        efficiency3=-costs.at["oil", "CO2 intensity"] * total_efficiency,
         capital_cost=costs.at["Fischer-Tropsch", "fixed"]
-        * 1
-        / costs.at["Fischer-Tropsch", "hydrogen-input"],  # EUR/MW_H2/a
-        marginal_cost=1
-        / costs.at["Fischer-Tropsch", "hydrogen-input"]
-        * costs.at["Fischer-Tropsch", "VOM"],
-        efficiency2=-costs.at["oil", "CO2 intensity"]
-        * 1
-        / costs.at["Fischer-Tropsch", "hydrogen-input"],
+        * total_efficiency,  # EUR/MW_H2/a
+        marginal_cost=total_efficiency * costs.at["Fischer-Tropsch", "VOM"],
         p_nom_extendable=True,
         p_min_pu=options["min_part_load_fischer_tropsch"],
         lifetime=costs.at["Fischer-Tropsch", "lifetime"],
@@ -3751,6 +3750,14 @@ def add_industry(n, costs):
         p_set=p_set,
     )
 
+    efficiency = round(
+        (13.8 + 1.9)
+        / (
+            costs.at["electric steam cracker", "naphtha-input"]
+            + costs.at["electric steam cracker", "electricity-input"]
+        ),
+        2,
+    )  # energy content of high and low value chemicals divided by energy input (all in /tHVC unit)
     n.madd(
         "Link",
         spatial.oil.kerosene,
@@ -3759,6 +3766,7 @@ def add_industry(n, costs):
         bus2="co2 atmosphere",
         carrier="kerosene for aviation",
         p_nom_extendable=True,
+        efficiency=efficiency,
         efficiency2=costs.at["oil", "CO2 intensity"],
     )
 
